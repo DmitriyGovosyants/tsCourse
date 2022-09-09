a53dc566463701b68b668db4b039cf218f468b0c
DmitriyGovosyants <Demiangvs@gmail.com>
2022-09-09 22:01:30 +0300

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+// const button = document.querySelector("button")!;
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
+
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// };
+
+// button.addEventListener("click", function() {
+//   console.log(add(+input1.value, +input2.value));
+// });
+
+// function combine(param1: string | number, param2: string | number) {
+//   if (typeof param1 === 'string' || typeof param2 === 'string') {
+//     return param1.toString() + param2.toString();
+//   }
+//   return param1 + param2;
+// }
+
+// combine(1, 2);
+
+// export {}
---
+++ src/buttonAction.ts
@@ -0,0 +1,8 @@
+const button2 = document.querySelector('button')!;
+
+if (button2) {
+  button2.addEventListener('click', () => {
+    console.log('Click');
+  });
+}
+// export {}
